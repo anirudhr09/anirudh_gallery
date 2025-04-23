931e34fafa2a64ed31a8fdbbc41fb76d58689881
anirudhr09 <75418611+anirudhr09@users.noreply.github.com>
2025-04-24 00:07:50 +0530

    Smooth photo loader

--- script.js
+++ script.js
@@ -15,14 +15,20 @@
 const imageList = ["about.jpg", "aboutDP.jpg", "AnirudhBeach.jpg", "AnirudhBench.jpg", "Avatar1.jpg", "bb black.jpg", "bb.jpg" ,"bb1.jpg" ,"bb1_cpy.jpg", "bb2.jpg", "bb2_cpy.jpg", "bb3.jpg", "bb4.jpg", "bb5.jpg", "bb6.jpg", "bb7.jpg", "bb8.jpg", "bb9.jpg", "Black_Court.jpg", "BlackBoardSide.jpg", "BLackFullCourt.jpg", "DP.jpg", "DP1.jpg", "dp3.jpg", "Profile Pic.jpg" 
   ];
   
-  const gallery = document.getElementById("gallery");
-  
-  imageList.forEach(image => {
-    const img = document.createElement("img");
-    img.src = `assets/${image}`;
-    img.alt = image;
-    gallery.appendChild(img);
-  });
+ const gallery = document.getElementById("gallery");
+
+imageList.forEach(image => {
+  const img = document.createElement("img");
+  img.src = `assets/${image}`;
+  img.alt = image;
+
+  // Wait for image to fully load before showing
+  img.onload = () => {
+    img.classList.add("loaded");
+  };
+
+  gallery.appendChild(img);
+});
   
   const toggleBtn = document.getElementById("toggleMode");
 
